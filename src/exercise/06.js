68446072d2baab67ae266fe88b8c7e77ef0695e4
Chris Peabody <chris.g.peabody@gmail.com>
2022-07-26 12:33:49 -0400

    Completed react-fundamentals!

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,26 +4,30 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const inputRef = React.useRef(null)
+  //const [error, setError] = React.useState(null)
+  const [username, setUsername] = React.useState(null)
 
-  // 🐨 add the onSubmit handler to the <form> below
+  function handleChange(event) {
+    const inputValue = event.target.value.toLowerCase()
+    //const isValid = inputValue === inputValue.toLowerCase()
+    //setError(isValid ? null : 'Username must be lower case')
+    setUsername(inputValue)
+  }
+  
+  function handleSubmit(event) {
+    // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+    event.preventDefault()
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+    const usernameInput = inputRef.current.value
+    onSubmitUsername(usernameInput)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input value={username} onChange={handleChange} ref={inputRef} id="usernameInput" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
